e6ae80a03360af4ca2f618cfea1cdb87cb1acebe
indoc4728-hub <indoc4728@gmail.com>
2026-06-12 10:14:15 +0700

    tahap 2 -tambah database

---
+++ db_latihan_pbo_trpl1a_mukhamad_ferdiyanto.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 12, 2026 at 03:10 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_latihan_pbo_trpl1a_mukhamad_ferdiyanto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabel_tiket`
+--
+
+CREATE TABLE `tabel_tiket` (
+  `id_tiket` int NOT NULL,
+  `nama_film` varchar(100) NOT NULL,
+  `jadwal_tayang` datetime NOT NULL,
+  `jumlah_kursi` int NOT NULL,
+  `harga_dasar_tiket` decimal(20,2) NOT NULL,
+  `jenis_studio` enum('Regular','IMAX','Velvet') NOT NULL,
+  `tipe_audio` varchar(50) DEFAULT NULL,
+  `lokasi_baris` varchar(100) DEFAULT NULL,
+  `kacamata_3d_id` varchar(100) DEFAULT NULL,
+  `efek_gerak_fitur` varchar(100) DEFAULT NULL,
+  `bantal_selimut_pack` varchar(100) DEFAULT NULL,
+  `layanan_sub_butler` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tabel_tiket`
+--
+ALTER TABLE `tabel_tiket`
+  ADD PRIMARY KEY (`id_tiket`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
